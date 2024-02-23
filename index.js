5f25c9aac6dcc842aa595c130de97110133a2af7
joaodev <jhtanajura@gmail.com>
2024-02-23 00:39:56 -0300

    meu primeiro commit

---
+++ index.js
@@ -0,0 +1,30 @@
+//DesafioDIOFelipão
+
+let heroi = "superman"
+
+var exp = 11111
+
+    if (exp <= 1000){
+        console.log("O herói de nome " + heroi + " está no nível de Ferro ")
+    }
+    else if (exp <= 2000){
+        console.log("O herói de nome " + heroi + " está no nível de Bronze ")
+    }
+    else if (exp <= 5000){
+        console.log("O herói de nome " + heroi + " está no nível de Prata ")
+    }
+    else if (exp <= 7000){
+        console.log("O herói de nome " + heroi + " está no nível de Ouro ")
+    }
+    else if (exp <= 8000){
+        console.log("O herói de nome " + heroi + " está no nível de Platina ")
+    }
+    else if (exp <= 9000){
+        console.log("O herói de nome " + heroi + " está no nível de Ascendente ")
+    }
+    else if (exp <= 1000){
+        console.log("O herói de nome " + heroi + " está no nível de Imortal ")
+    }
+    else if (exp > 10000){
+        console.log("O herói de nome " + heroi + " está no nível de Radiante ")
+    }
